aaf64fb8910c6812043400c5523bc309c0a403a4
miguelraad28 <miguelraad2020@gmail.com>
2022-06-25 11:21:22 -0300

    Fibonacci number

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//////////////// DO WHILE //////////////////////
+let numero1, numero2, acumulador, nombre
 
-let numero1, numero2, operacion
-
+nombre = prompt("Bienvenido, cómo te llamas?");
+alert("Con este pequeño ciclo, sabrás la suma de todos los numeros de un rango.");
 do{
-    numero1 = parseFloat(prompt("Ingrese un numero"));
-    numero2 = parseFloat(prompt("Ingrese otro numero"));
-    operacion = prompt("Ingrese que operacion quiere realizar. (+, -, *, /)")
+    numero1 = parseFloat(prompt("Primer número del rango a sumar:"));
+    numero2 = parseFloat(prompt("Último número del rango a sumar:"));
+    if(isNaN(numero1) || isNaN(numero2)){
+        alert("Necesito que indiques valores numéricos.")
+    }
+    else{
+    }
 }
-while(isNaN(numero1) || isNaN(numero2))
-switch(operacion){
-    case "+":
-        alert(numero1 + numero2);
-        break;
-    case "-":
-        alert(numero1 - numero2);
-        break;
-    case "*":
-        alert(numero1 * numero2);
-        break;
-    case "/":
-        alert(numero1 / numero2);
-        break;
-    default:
-        alert("Operacion no valida");
-        break;
+while(isNaN(numero1) || (isNaN(numero2))){
+    acumulador = 0
+    for(numero1 = numero1; numero1 <= numero2; numero1++){
+    acumulador += numero1;
+    }
+    alert(`${nombre}, la suma del rango de numeros es igual a ${acumulador}`);
 }
