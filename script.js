d5a23d3e64ff44bb0533ac49e80521faea8e363c
miguelraad28 <miguelraad2020@gmail.com>
2022-07-08 16:07:16 -0300

    Dolor de cabeza

--- script.js
+++ script.js
@@ -1,71 +1,61 @@
-let nombre, apellido, dni, ingresosMensuales, prestamoMaximo, sumaPagoMaximo, pagoMaximo, interesesTotales, pagoTotal, cuotasMinimas, cuotas, pagoCuota, respuesta, condicion1, condicion2
-function funcionInteresesCuotas(cuotas, montoPrestamo) {
-    let montoPrestamoDuplicado = montoPrestamo
-    if(cuotas === 1 || cuotas < 6){
-        interesesTotales = (montoPrestamo * 1.10) - montoPrestamoDuplicado;
-    }else if(cuotas === 6 || cuotas < 9){
-        interesesTotales = (montoPrestamo * 1.13) - montoPrestamoDuplicado;
-    }else if(cuotas === 9 || cuotas < 12){
-        interesesTotales = (montoPrestamo * 1.17) - montoPrestamoDuplicado;
-    }else if(cuotas === 12 || cuotas < 19){
-        interesesTotales = (montoPrestamo * 1.22) - montoPrestamoDuplicado;
-    }else{
-        interesesTotales = (montoPrestamo * 1.30) - montoPrestamoDuplicado;
+// Constructor
+class Producto{
+    constructor(nombre, tipo, marca, precio, stock, sku){
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.marca = marca;
+    this.precio = precio;
+    this.stock = stock;
+    this.sku = sku;
     }
-    return interesesTotales;
 }
-alert("Simulador de préstamo. Solicitud de hasta 4 veces tus ingresos y pago máximo mensual del 30%.");
-nombre = prompt("Ingrese su nombre");
-do {
-    dni = parseInt(prompt("Ingrese su Nº de DNI"));
-    ingresosMensuales = parseFloat(prompt("Ingrese sus ingresos mensuales"));
-    if((dni < 1000000 || dni > 100000000) || isNaN(dni)){
-        alert("Ingrese el DNI correctamente");
-    }else{}
-    if(isNaN(ingresosMensuales)){
-        alert("Ingrese correctamente sus ingresos");
-    }else if(ingresosMensuales < 40000){
-        alert("Sus ingresos mensuales mínimos para acceder al prestamo deben superar los $40000 pesos");
-    }else{
-        prestamoMaximo = ingresosMensuales * 4;
-        pagoMaximo = ingresosMensuales * 0.30;
+// Objetos
+const producto1 = new Producto("Oráculo conexión con el universo","mazos","Infusa",2890,70,1)
+const producto2 = new Producto("Oráculo un tecito para el alma","mazos","Infusa",2420,50,2)
+const producto3 = new Producto("Oráculo angelical","mazos","Infusa",3250,35,3)
+const producto4 = new Producto("Confeti de cuarzos","piedras","Infusa",2500,12,4)
+const producto5 = new Producto("Confeti de cuarzos especial","piedras","Infusa",2900,8,5)
+const producto6 = new Producto("Kit 7 Chakras","limpieza energetica","Infusa",2200,20,6)
+const producto7 = new Producto("Ejemploº7","limpieza energetica","Infusa",500,22,7)
+const producto8 = new Producto("Ejemploº8","piedras","Infusa",900,15,8)
+const producto9 = new Producto("Ejemploº9","mazos","Infusa",1900,45,9)
+// Array de objetos
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9]
+//Funcion de orden de precio
+function ordenarPrecio(productos){
+    consulta = parseInt(prompt("Si quieres te puedo ordenar de menor a mayor o viceversa. Ingrese 1 para ordenar de menor a mayor, 2 para ordenar de mayor a menor"))
+    if(consulta === 1){
+        const precioMenorMayor = productos
+        precioMenorMayor.sort((a,b) => a.precio-b.precio)
+        precioMenorMayor.forEach(producto =>{
+            console.log(producto)
+        })
+    }else if(consulta === 2){
+        const precioMayorMenor = productos
+        precioMayorMenor.sort((a,b) => b.precio-a.precio)
+        precioMayorMenor.forEach(producto =>{
+            console.log(producto)
+        })
     }
-} while ((isNaN(dni) || isNaN(dni)) || ((dni < 1000000) || (dni > 100000000)) || ((isNaN(ingresosMensuales)) || (ingresosMensuales < 40000)));
-do {
-    montoPrestamo = parseFloat(prompt(`¿Qué cantidad desea solicitar? Su máximo es de $${prestamoMaximo}`));
-    if(montoPrestamo > prestamoMaximo){
-        alert(`Su préstamo no puede exceder ${prestamoMaximo}`);
-        condicion1 = true
-    }else{
-        condicion1 = false
-    }
-    for(sumaPagoMaximo = pagoMaximo; sumaPagoMaximo <= montoPrestamo; sumaPagoMaximo += pagoMaximo){
-        if(sumaPagoMaximo <= montoPrestamo){
-            cuotasMinimas = sumaPagoMaximo / pagoMaximo
-        }else{}
-    }
-} while (condicion1);
-do {
-    cuotas = parseInt(prompt(`¿En cuantas cuotas desea realizar el pago? Minimas de ${cuotasMinimas} y el máximo es de 24`));
-    if(cuotas < cuotasMinimas){
-        alert(`Las cuotas minimas son de ${cuotasMinimas}`);
-    }else if(isNaN(cuotas)){
-        alert("Por favor ingrese las cuotas en valor numérico");
-    }else if(cuotas > 24){
-        alert("El máximo de cuotas es de 24")
-    }else{
-        funcionInteresesCuotas(cuotas, montoPrestamo);
-        pagoTotal = montoPrestamo + interesesTotales
-        pagoCuota = pagoTotal / cuotas
-        condicion2 = false;
-    }
-    alert(`${nombre}, su préstamo de $${montoPrestamo}, con intereses de $${interesesTotales}, le queda en un total de $${pagoTotal} en ${cuotas} cuotas de $${pagoCuota}`);
-    respuesta = prompt("Si esta de acuerdo con el prestamo, ingrese si").toLowerCase();
-    if(respuesta === "si"){
-        alert("Perfecto, pronto se te depositará");
-    }else{
-        alert("Bueno, en otro momento");
-    }
-} while (condicion2);
-
-
+}
+// Filtro por tipo de producto
+let consulta = prompt("Desea filtrar por mazos, piedras o limpieza energetica? Escriba su respuesta").toLowerCase()
+if(consulta === "mazos"){
+    const productosMazos = productos.filter(producto => producto.tipo === "mazos")
+    productosMazos.forEach(producto => {
+    console.log(producto)
+})
+}else if(consulta === "piedras"){
+    const productosPiedras = productos.filter(producto => producto.tipo === "piedras")
+    productosPiedras.forEach(producto => {
+    console.log(producto)
+})
+}else if(consulta === "limpieza energetica" || consulta === "limpieza"){
+    const productosLimpieza = productos.filter(producto => producto.tipo === "limpieza energetica")
+    productosLimpieza.forEach(producto => {
+        console.log(producto)
+    })
+}else{
+    alert("No entendí tu filtración. Te ofrezco otro tipo de ordenamiento.")
+    ordenarPrecio(productos)
+}
