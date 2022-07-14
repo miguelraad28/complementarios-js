03e3755bcc12825204a92ade155c950c80514507
miguelraad28 <miguelraad2020@gmail.com>
2022-07-14 14:55:06 -0300

    a

--- script.js
+++ script.js
@@ -1,39 +1,85 @@
-class Producto {
-    constructor(id, nombre, precio, stock, categoria, imagen){
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-        this.stock = stock
-        this.categoria = categoria
-        this.imagen = imagen
+//Nombre de tapas:
+    //un array de productos(cada objeto con su nombre y precio) y un array vacio.
+    const productos = [ 
+    
+        { nombre : "Floral", precio: 1000} ,
+        { nombre : "Costelacion", precio: 800} ,
+        { nombre : "Ciudad", precio: 1200} ,
+        { nombre : "Bosque", precio: 1400} ,
+        { nombre : "Comidas", precio: 600} ,
+    ]
+
+
+    let respuesta
+    
+   do {
+       respuesta = prompt("Responda si o no ¿Quiere comprar un azafran?").toLowerCase()
+        if(respuesta == "si"){
+            alert("Te muestro mis productos por consola!")
+            productos.forEach(producto => {
+                console.log(producto)
+            })
+        }else if(respuesta =="no"){
+            alert("En otra ocasión")
+        }else{
+            alert("Disculpa, no entendí tu respuesta.")
+        }
+       }
+   while (respuesta != "si")
+ /*     alert (todosLosProductos.join(" - "))
+     }
+   (seleccionar == "no" ) {
+    alert ("gracias por visitarnos!")
     }
-}
-const producto1 = new Producto(1, "Oráculo conexión con el universo", 2850, 22, "mazos", "images/productos/cartas/universosolo.jpg")
-const producto2 = new Producto(2, "Oráculo conexión con el universo", 2850, 22, "mazos")
-const producto3 = new Producto(3, "BOX - Oráculo conexión con el universo", 3950, 9, "mazos")
-const producto4 = new Producto(4, "Oráculo tecito para el alma", 2450, 5, "mazos")
-const producto5 = new Producto(5, "Oráculo Angelical", 3220, 35, "mazos")
-const producto6 = new Producto(6, "Consulta de tarot 50min", 4990, 10000, "servicio")
-const producto7 = new Producto(7, "Kit Limpieza 7 Chakras", 2590, 15, "limpieza")
-const producto8 = new Producto(8, "Spry Abundancia y Fortuna", 650, 12, "limpieza")
-const producto9 = new Producto(9, "Spry Limpieza Energética", 550, 15, "limpieza")
-const producto10 = new Producto(10, "Spry Ruda y Romero", 750, 20, "limpieza")
-const producto11 = new Producto(11, "Vela de soja de la Abundancia", 950, 9, "velas")
-const producto12 = new Producto(12, "Vela de soja para el Amor", 1050, 15, "velas")
-const producto13 = new Producto(13, 'Vela de soja "Buda"', 820, 2, "velas")
-const producto14 = new Producto(14, "Vela de soja de Limpieza", 920, 5, "velas")
-const producto15 = new Producto(15, "Vela de soja de Seducción", 1200, 6, "velas")
-const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12, producto13, producto14, producto15]
+    
 
-let misProductosIndex = document.getElementById("div-padre-productos");
+    //creo un bucle con while para que pueda elegir entre los productos
+    //y una variable cantidad nueva para que ingrese numeros de cuadernos.
+   while (seleccionar != "no")
+  {
+    let producto = prompt("Agrega un azafrán a tu carrito")
+    let precio = 0 
 
-for(let producto of productos){
-    misProductosIndex.innerHTML += `
-    <div class="div-producto>
-    <div><img src="${producto.imagen}"></div>
-    <h3>${producto.nombre}</h3>
-    <p>${producto.precio}</p>
-    <p>¡Quedan ${producto.stock} disponibles!</p>
-    </div>
-    `
+    while (producto == "Floral" || producto == "Constelacion"||producto == "Bosque"|| producto == "Ciudad"|| producto == "Comidas")
+    switch(producto) {
+      case "Floral": precio = 1000;
+      break;
+      case "Constelacion": precio = 800;
+      break;
+      case "Bosque": precio = 1200;
+      break;
+      case "Ciudad": precio = 1400;
+      break;
+      case "Comidas": precio = 600;
+      break;
+      default:
+        break;
+    }
+
+    let cantidad = parseInt(prompt("¿cuantos azafranes deseas?"))
+
+carrito.push ({producto,precio}) //las llaves son para que me queden en forma de objeto.
+console.log(carrito)
+
 }
+else
+
+let producto
+ if (producto != "Floral" || producto != "Constelacion"||producto != "Bosque"|| producto !="Ciudad"|| producto != "Comidas") { {
+    alert( "Por favor seleccione un producto en stock");
+  }
+
+  seleccionar = prompt ("¿Quiere agregar algo mas al carrito?")
+
+  while (seleccionar === "no") { 
+  alert("Gracias por tu compra")
+  //muestro lo que compra:
+  carrito.forEach((carritocuenta) => {
+    console.log (carritocuenta.cantidad * carritocuenta.precio);
+  })
+  break;
+ }
+
+
+}
+  */
