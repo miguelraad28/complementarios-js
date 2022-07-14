676ecf6d07c731d5093f187fd8abcbd0cf82ca45
miguelraad28 <miguelraad2020@gmail.com>
2022-07-14 12:40:15 -0300

    conectando con el dom. productos dinámicos

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-asd
+for(let producto of productos){
+    misProductos.innerHTML += `
+        <div class="div-producto">
+        <div><img src="${producto.imagen}"></div>
+        <h3>${producto.nombre}</h3>
+        <p>${producto.precio}</p>
+        <p>${producto.stock}</p>
+        </div>
+    `
+}
