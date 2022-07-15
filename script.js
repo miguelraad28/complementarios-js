cbbeb65290967202e38df912b1ba525ef742d2a2
miguelraad28 <miguelraad2020@gmail.com>
2022-07-15 11:50:00 -0300

    trabajo robado

--- script.js
+++ script.js
@@ -1,119 +1,104 @@
 //Nombre de tapas:
     //un array de productos(cada objeto con su nombre y precio) y un array vacio.
-    class Producto{
-        constructor(nombre, precio){
-            this.nombre = nombre
-            this.precio = precio
-        }
-    }
-    class ProductoEnCarrito{
-        constructor(nombre, precio, cantidad){
+    class Productos{
+        constructor(nombre, precio, cantidad = 0){
             this.nombre = nombre
             this.precio = precio
             this.cantidad = cantidad
         }
-        añadirProducto(sumarCantidad){
-        this.cantidad = sumarCantidad
+        añadirCantidad(cantidadAComprar){
+            if(this.cantidad > 0){
+                this.cantidad += cantidadAComprar
+            }else if(this.cantidad == 0){
+                this.cantidad = cantidadAComprar
+            }
         }
     }
-    const producto1 = new Producto("Floral", 1000)
-    const producto2 = new Producto("Constelación", 800)
-    const producto3 = new Producto("Ciudad", 1200)
-    const producto4 = new Producto("Bosque", 1400)
-    const producto5 = new Producto("Comidas", 600)
+    const producto1 = new Productos("floral", 1000)
+    const producto2 = new Productos("constelación", 800)
+    const producto3 = new Productos("ciudad", 1200)
+    const producto4 = new Productos("bosque", 1400)
+    const producto5 = new Productos("comidas", 600)
 
     const productos = [producto1, producto2, producto3, producto4, producto5]
+    const productosAMostrar = productos.map(producto => producto ={nombre: producto.nombre, precio: producto.precio})
     const carrito = []
     let respuesta
     
-   do {
-       respuesta = prompt("Responda si o no ¿Quiere comprar un azafran?").toLowerCase()
-        if(respuesta == "si"){
-            alert("Te muestro mis productos por consola!")
-            productos.forEach(producto => {
-                console.log(producto)
-            })
-        }else if(respuesta =="no"){
-            alert("En otra ocasión")
-        }else{
-            alert("Disculpa, no entendí tu respuesta.")
+    do {
+        respuesta = prompt("Responda si o no ¿Quiere comprar un azafran?").toLowerCase()
+            if(respuesta == "si"){
+                    alert("Te muestro mis productos por consola!")
+                    productosAMostrar.forEach(producto => {
+                    console.log(producto)
+                })
+            }else if(respuesta =="no"){
+                alert("En otra ocasión")
+            }else{
+                alert("Disculpa, no entendí tu respuesta.")
+            }
         }
-       }
-   while (respuesta != "si")
-
-   let respuestaProducto
+    while (respuesta != "si")
+    let respuestaProducto
     let respuestaAñadirMas
     let cantidadAComprar
+    do{
+    respuestaProducto = prompt("que producto quiere añadir?").toLowerCase()
+        switch(respuestaProducto) {
+        case "floral":
+            if(carrito.some(producto => producto = (producto.nombre == "floral"))){
+                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
+                productos[0].añadirCantidad(cantidadAComprar)
+            }else{
+                carrito.push(productos[0])
+                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
+                productos[0].añadirCantidad(cantidadAComprar)
+            }
+            break;
+        case "constelacion":
+            if(carrito.some(producto => producto = (producto.nombre == "constelacion"))){
+                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
+                productos[1].añadirCantidad(cantidadAComprar)
+            }else{
+                carrito.push(productos[1])
+                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
+                productos[1].añadirCantidad(cantidadAComprar)
+            }
+            break;
+        case "bosque":
+            if(carrito.some(producto => producto = (producto.nombre == "bosque"))){
+                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
+                productos[2].añadirCantidad(cantidadAComprar)
+            }else{
+                carrito.push(productos[2])
+                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
+                productos[2].añadirCantidad(cantidadAComprar)
+            }
+            break;
+        case "ciudad":
+            if(carrito.some(producto => producto = (producto.nombre == "ciudad"))){
+                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
+                productos[3].añadirCantidad(cantidadAComprar)
+            }else{
+                carrito.push(productos[3])
+                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
+                productos[3].añadirCantidad(cantidadAComprar)
+            }
+            break;
+        case "comidas":
+            if(carrito.some(producto => producto = (producto.nombre == "comidas"))){
+                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
+                productos[4].añadirCantidad(cantidadAComprar)
+            }else{
+                carrito.push(productos[4])
+                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
+                productos[4].añadirCantidad(cantidadAComprar)
+            }
+            break;
+        default:
+            break;
+        }
 
-   do{
-   respuestaProducto = prompt("que producto quiere añadir?").toLowerCase()
-    
-   switch(respuestaProducto) {
-    case "floral": const productoEnCarrito1 = new ProductoEnCarrito("Floral", 1000, 0)
-    carrito.push(productoEnCarrito1)
-    cantidadAComprar = prompt("Cuantos queres comprar?")
-    productoEnCarrito1.añadirProducto(cantidadAComprar)
-    break;
-    case "constelacion": const productoEnCarrito2 = new ProductoEnCarrito("Constelacion", 800, 0)
-    carrito.push(productoEnCarrito2)
-    cantidadAComprar = prompt("Cuantos queres comprar?")
-    productoEnCarrito2.añadirProducto(cantidadAComprar)
-    break;
-    case "bosque": const productoEnCarrito3 = new ProductoEnCarrito("Ciudad", 1200, 0)
-    carrito.push(productoEnCarrito3)
-    cantidadAComprar = prompt("Cuantos queres comprar?")
-    productoEnCarrito3.añadirProducto(cantidadAComprar)
-    break;
-    case "ciudad": const productoEnCarrito4 = new ProductoEnCarrito("Bosque", 1400, 0)
-    carrito.push(productoEnCarrito4)
-    cantidadAComprar = prompt("Cuantos queres comprar?")
-    productoEnCarrito4.añadirProducto(cantidadAComprar)
-    break;
-    case "comidas": const productoEnCarrito5 = new ProductoEnCarrito("Comidas", 600, 0)
-    carrito.push(productoEnCarrito5)
-    cantidadAComprar = prompt("Cuantos queres comprar?")
-    productoEnCarrito5.añadirProducto(cantidadAComprar)
-    break;
-    default:
-      break;
-  }
-
-  respuestaAñadirMas = prompt("Desea añadir otro producto?")
+        respuestaAñadirMas = prompt("Desea añadir otro producto?")
     }while(respuestaAñadirMas !="no")
-  console.log(carrito)
-/*
-   while (seleccionar != "no")
-  {
-    let producto = prompt("Agrega un azafrán a tu carrito")
-    let precio = 0 
-
-    while (producto == "Floral" || producto == "Constelacion"||producto == "Bosque"|| producto == "Ciudad"|| producto == "Comidas")
-
-
-    let cantidad = parseInt(prompt("¿cuantos azafranes deseas?"))
-
-carrito.push ({producto,precio}) //las llaves son para que me queden en forma de objeto.
 console.log(carrito)
-
-}
-else
-
-let producto
- if (producto != "Floral" || producto != "Constelacion"||producto != "Bosque"|| producto !="Ciudad"|| producto != "Comidas") { {
-    alert( "Por favor seleccione un producto en stock");
-  }
-
-  seleccionar = prompt ("¿Quiere agregar algo mas al carrito?")
-
-  while (seleccionar === "no") { 
-  alert("Gracias por tu compra")
-  //muestro lo que compra:
-  carrito.forEach((carritocuenta) => {
-    console.log (carritocuenta.cantidad * carritocuenta.precio);
-  })
-  break;
- }
-
-
-}
-  */
