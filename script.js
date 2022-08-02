660594b47b8b97886be1e4651e4f7110d01ffa7a
miguelraad28 <miguelraad2020@gmail.com>
2022-08-02 12:42:37 -0300

    ternario, spread, alias, desestructuraci´ón, librerías

--- script.js
+++ script.js
@@ -1,104 +1,68 @@
-//Nombre de tapas:
-    //un array de productos(cada objeto con su nombre y precio) y un array vacio.
-    class Productos{
-        constructor(nombre, precio, cantidad = 0){
-            this.nombre = nombre
-            this.precio = precio
-            this.cantidad = cantidad
-        }
-        añadirCantidad(cantidadAComprar){
-            if(this.cantidad > 0){
-                this.cantidad += cantidadAComprar
-            }else if(this.cantidad == 0){
-                this.cantidad = cantidadAComprar
-            }
-        }
+let usuarios = [];
+(localStorage.getItem("usuarios")) ? (usuarios = JSON.parse(localStorage.getItem("usuarios"))) : (localStorage.setItem("usuarios", JSON.stringify(usuarios)))
+class Usuario{
+    constructor(email, user, contraseña){
+        this.email = email
+        this.user = user
+        this.contraseña = contraseña
     }
-    const producto1 = new Productos("floral", 1000)
-    const producto2 = new Productos("constelación", 800)
-    const producto3 = new Productos("ciudad", 1200)
-    const producto4 = new Productos("bosque", 1400)
-    const producto5 = new Productos("comidas", 600)
-
-    const productos = [producto1, producto2, producto3, producto4, producto5]
-    const productosAMostrar = productos.map(producto => producto ={nombre: producto.nombre, precio: producto.precio})
-    const carrito = []
-    let respuesta
-    
-    do {
-        respuesta = prompt("Responda si o no ¿Quiere comprar un azafran?").toLowerCase()
-            if(respuesta == "si"){
-                    alert("Te muestro mis productos por consola!")
-                    productosAMostrar.forEach(producto => {
-                    console.log(producto)
-                })
-            }else if(respuesta =="no"){
-                alert("En otra ocasión")
-            }else{
-                alert("Disculpa, no entendí tu respuesta.")
-            }
+}
+const formularioCrearCuenta = document.getElementById("formCrearCuenta")
+const errorCrearCuenta = document.getElementById("errorCrearCuenta")
+formularioCrearCuenta.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    let datosNuevoUsuario = new FormData(e.target)
+    if(datosNuevoUsuario.get("newUserEmail") == ""){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No puedes crear una cuenta sin un Email!',
+        })
+    }else if(usuarios.some(usuario => usuario.email === datosNuevoUsuario.get("newUserEmail"))){
+        Swal.fire({
+            icon: 'info',
+            title: 'Oops...',
+            text: `Parece que ya tienes una cuenta registrada con ${datosNuevoUsuario.get("newUserEmail")}`,
+        })
+    }else if(datosNuevoUsuario.get("newUserUsername") == ""){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes crear un nombre de usuario!',
+        })
+    }else if(usuarios.some(usuario => usuario.user === datosNuevoUsuario.get("newUserUsername"))){
+        Swal.fire({
+            icon: 'info',
+            title: 'Oops...',
+            text: 'Ya hay una cuenta creada con ese usuario, prueba otro.',
+        })
+    }else if(datosNuevoUsuario.get("newUserPassword1") !== datosNuevoUsuario.get("newUserPassword2")){
+        Swal.fire({
+            icon: 'warning',
+            title: 'Oops...',
+            text: 'Atento, las contraseñas no coinciden!',
+        })
+    }else if(datosNuevoUsuario.get("newUserPassword1") == "" || datosNuevoUsuario.get("newUserPassword2") == ""){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes crear una contraseña!',
+        })
+    }else{
+        let nuevoUsuario = new Usuario (datosNuevoUsuario.get("newUserEmail"), datosNuevoUsuario.get("newUserUsername"), datosNuevoUsuario.get("newUserPassword1"))
+        // SPREAD
+        let usuarioAAñadir = {
+            ...nuevoUsuario
         }
-    while (respuesta != "si")
-    let respuestaProducto
-    let respuestaAñadirMas
-    let cantidadAComprar
-    do{
-    respuestaProducto = prompt("que producto quiere añadir?").toLowerCase()
-        switch(respuestaProducto) {
-        case "floral":
-            if(carrito.some(producto => producto = (producto.nombre == "floral"))){
-                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
-                productos[0].añadirCantidad(cantidadAComprar)
-            }else{
-                carrito.push(productos[0])
-                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
-                productos[0].añadirCantidad(cantidadAComprar)
-            }
-            break;
-        case "constelacion":
-            if(carrito.some(producto => producto = (producto.nombre == "constelacion"))){
-                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
-                productos[1].añadirCantidad(cantidadAComprar)
-            }else{
-                carrito.push(productos[1])
-                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
-                productos[1].añadirCantidad(cantidadAComprar)
-            }
-            break;
-        case "bosque":
-            if(carrito.some(producto => producto = (producto.nombre == "bosque"))){
-                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
-                productos[2].añadirCantidad(cantidadAComprar)
-            }else{
-                carrito.push(productos[2])
-                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
-                productos[2].añadirCantidad(cantidadAComprar)
-            }
-            break;
-        case "ciudad":
-            if(carrito.some(producto => producto = (producto.nombre == "ciudad"))){
-                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
-                productos[3].añadirCantidad(cantidadAComprar)
-            }else{
-                carrito.push(productos[3])
-                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
-                productos[3].añadirCantidad(cantidadAComprar)
-            }
-            break;
-        case "comidas":
-            if(carrito.some(producto => producto = (producto.nombre == "comidas"))){
-                cantidadAComprar = parseInt(prompt("Cuantos más desea comprar?"))
-                productos[4].añadirCantidad(cantidadAComprar)
-            }else{
-                carrito.push(productos[4])
-                cantidadAComprar = parseInt(prompt("Cuantos desea comprar?"))
-                productos[4].añadirCantidad(cantidadAComprar)
-            }
-            break;
-        default:
-            break;
-        }
-
-        respuestaAñadirMas = prompt("Desea añadir otro producto?")
-    }while(respuestaAñadirMas !="no")
-console.log(carrito)
+        usuarios.push(usuarioAAñadir)
+        localStorage.setItem("usuarios", JSON.stringify(usuarios))
+        formularioCrearCuenta.reset()
+        // DESESTRUCTURACION Y ALIAS
+        let {email:e, user:u} = nuevoUsuario
+        Swal.fire({
+            icon: 'success',
+            title: '¡Excelente!',
+            text: `${u}, ya tienes tu cuenta en nuestra web, ingresa al mail ${e} y verifica tu cuenta para ingresar!`,
+        })
+    }
+})
